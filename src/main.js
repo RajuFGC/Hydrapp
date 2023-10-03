5a38839e1008bfa52517efb808a80fa8caf687ee
Wojciech Majewski <holyordersol84@gmail.com>
2023-10-03 15:11:03 +0200

    added js

---
+++ src/main.js
@@ -0,0 +1,28 @@
+let count = 0;
+const addButton = document.querySelector('.addGlass--js');
+const removeButton = document.querySelector('.removeGlass--js')
+const countChecker = document.querySelector('.counter--js')
+const key = new Date(2023,9,4). toISOString().slice(0,10)
+const storedCount = localStorage.getItem(key);
+
+if (storedCount !== null) {
+  count = parseInt(storedCount);
+  countChecker.textContent = count;
+}
+
+addButton.addEventListener('click',() => {
+count++;
+countChecker.textContent = count;
+localStorage.setItem(key, count.toString())
+});
+removeButton.addEventListener('click',() => {
+if (count>0) {
+  count--;
+  countChecker.textContent = count;
+  localStorage.setItem(key, count.toString())
+}
+  });
+
+
+  
+  
