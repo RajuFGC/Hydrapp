00f18e95454246e94d8ce993f07957600ca32687
Wojciech Majewski <holyordersol84@gmail.com>
2023-10-10 13:15:32 +0200

    added local time and set date/key change after time(15sec)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ let count = 0;
 const addButton = document.querySelector('.addGlass--js');
 const removeButton = document.querySelector('.removeGlass--js')
 const countChecker = document.querySelector('.counter--js')
-const key = new Date(). toISOString().slice(0,10)
+let key = new Date(). toLocaleString().slice(0,10)
 const storedCount = localStorage.getItem(key);
+
+const setCounterValue = (value) => {
+  countChecker.innerHTML = value;
+  localStorage.setItem(key, value);
+}
+
+setCounterValue(count);
+
+setInterval(() => {
+  console.log('tik-tak')
+  if (key !== new Date(). toLocaleString().slice(0,10)) {
+    key = new Date(). toLocaleString().slice(0,10)
+    setCounterValue(0)
+    count = 0;
+  }
+}, 1000*5 );
 
 if (storedCount !== null) {
   count = parseInt(storedCount);
@@ -13,13 +29,13 @@ if (storedCount !== null) {
 addButton.addEventListener('click',() => {
 count++;
 countChecker.textContent = count;
-localStorage.setItem(key, count.toString())
+localStorage.setItem(key, count.toLocaleString())
 });
 removeButton.addEventListener('click',() => {
 if (count>0) {
   count--;
   countChecker.textContent = count;
-  localStorage.setItem(key, count.toString())
+  localStorage.setItem(key, count.toLocaleString())
 }
   });
 
